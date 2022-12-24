4708075a80b3fcd2cd18d807b6f685f57ad97162
David Malpica <dmalpica96@hotmail.com>
2022-12-24 14:36:45 -0600

    Add data on service

--- src/app/articulos.service.ts
+++ src/app/articulos.service.ts
@@ -6,4 +6,11 @@ import { Injectable } from '@angular/core';
 export class ArticulosService {
 
   constructor() { }
+  retornar(){
+    return [{codigo:1,descripcion:"papas",precio:10.55},
+            {codigo:2,descripcion:"manzanas",precio:12.10},
+            {codigo:3,descripcion:"melon",precio:52.30},
+            {codigo:4,descripcion:"cebollas",precio:17},
+            {codigo:5,descripcion:"calabaza",precio:20}]
+  }
 }
