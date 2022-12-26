5c9f7bf76ee381052d5075697293985708866d06
David Malpica <dmalpica96@hotmail.com>
2022-12-26 13:04:36 -0600

    Create table in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,56 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.articulos = this.articulosServicio.retornar();
   }
+
+  art = {
+    codigo:0 ,
+    descripcion:"",
+    precio:0
+  }
+
+  hayRegistro(){
+    return this.articulos.length>0;
+  }
+
+  borrar(codigo:number) {
+    for(let x=0;x<this.articulos.length;x++)
+      if(this.articulos[x].codigo==codigo) {
+        this.articulos.splice(x,1);
+        return;
+      }
+  }
+
+  agregar(){
+    if(this.art.codigo==0){
+      alert('El articulo debe ser distinto de 0');
+      return;
+    }
+    for(let x=0;x<this.articulos.length;x++)
+    if (this.articulos[x].codigo==this.art.codigo)
+    {
+      alert('Articulo existente');
+      return;
+    }    
+    this.articulos.push({codigo:this.art.codigo, descripcion: this.art.descripcion, precio:this.art.precio});
+    this.art.codigo=0;
+    this.art.descripcion='';
+    this.art.precio=0;
+  }
+
+  seleccionar(art:{codigo:number;descripcion:string;precio:number}){
+    this.art.codigo = art.codigo;
+    this.art.descripcion = art.descripcion;
+    this.art.precio = art.precio;
+  }
+
+  modificar(){
+    for(let x=0;x<this.articulos.length;x++)
+      if (this.articulos[x].codigo==this.art.codigo)
+      {
+        this.articulos[x].descripcion=this.art.descripcion;
+        this.articulos[x].precio=this.art.precio;
+        return;
+      }        
+    alert('El articulo no existe');
+  }
 }
